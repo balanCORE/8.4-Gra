8d56f894bb440b84f5097f900ad752637111e4a5
kieUba <bartosz.gargol.dev@gmail.com>
2018-12-13 20:36:00 +0100

    mentor please

--- js/script.js
+++ js/script.js
@@ -6,54 +6,55 @@ var rockChoice = document.getElementById('btnRock');
 var scisorsChoice = document.getElementById('btnScisors');
 var paperChoice = document.getElementById('btnPaper');
 
-function myPoint(point) {
-	playerSumPoints.sum++;
-	return addPointPlayer.innerHTML ++;
-};
-
-function computerPoint(point) {
-	computerSumPoints.sum ++;
-	return addPointComputer.innerHTML ++;
-};
-
-rockChoice.addEventListener('click', function(){
-	if (randomChoice() == 'rock') return myPoint() + computerPoint();
-	else if (randomChoice() == 'scissors') return myPoint();
-	else return computerPoint()
-	checkResults();
-});
-
-scisorsChoice.addEventListener('click', function(){
-	if (randomChoice() == 'rock') return computerPoint();
-	else if (randomChoice() == 'scissors') return myPoint() + computerPoint();
-	else return myPoint();
-	checkResults();
-});
-
-paperChoice.addEventListener('click', function(){
-	if (randomChoice() == 'rock') return myPoint();
-	else if (randomChoice() == 'scissors') return computerPoint();
-	else return computerPoint() + myPoint();
-	checkResults();
-});
-
-function randomChoice(){
-	var computerChoice = ['rock','scissors','paper'];
-	return computerChoice[Math.floor(Math.random() * 3)];
-
-};
-
 // zapisanie wyników
-var playerSumPoints = {sum: 0};
-var computerSumPoints = {sum: 0};
+var playerSum = {sum: 0, choice: ' '};
+var computerSum = {sum: 0, choice: ' '};
 
+// klikacze
+rockChoice.addEventListener('click', function() {
+	playerChoice('rock')
+});
+
+scisorsChoice.addEventListener('click', function() {
+	playerChoice('scisors')
+});
+
+paperChoice.addEventListener('click', function() {
+	playerChoice('paper')
+});
+
+function playerChoice(choice){
+	playerSum.choice = choice;
+	computerSum.choice = randomChoice();
+	compare();
+	showResults();
+	checkResults();
+}
+
+function randomChoice() {
+	var computerChoice = ['rock','scisors','paper'];
+	return computerChoice[Math.floor(Math.random() * 3)]
+};
+
+function compare(){
+	if(playerSum.choice === computerSum.choice) { playerSum.sum++ + computerSum.sum++; }
+	else if((playerSum.choice === 'rock' && computerSum.choice === 'scissors') || (playerSum.choice === 'paper' && computerSum.choice === 'rock') || (playerSum.choice === 'scissors' && computerSum.choice === 'paper')) {
+	playerSum.score++;}
+	else { computerSum.score++; }
+};
+
+function showResults() {
+	addPointPlayer.innerHTML = playerSum.sum;
+	addPointComputer.innerHTML = computerSum.sum;
+};
 
 function checkResults() {
-	if (playerSumPoints.sum == 10) {
-		addPointPlayer = "YOU";
-		addPointComputer = "WON!"
-	} else if (computerSumPoints.sum == 10) {
-		addPointPlayer = "YOU";
-		addPointComputer = "LOST!!!!!!"
+	if (playerSum.sum == 10) {
+		addPointPlayer.innerHTML = "YOU";
+		addPointComputer.innerHTML = "WON!"
+	} else if (computerSum.sum == 10) {
+		addPointPlayer.innerHTML = "YOU";
+		addPointComputer.innerHTML = "LOST!!"
 	}
 }
+
